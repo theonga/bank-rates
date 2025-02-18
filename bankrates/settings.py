150e2092d32caa90919b6fb5a8b3e2df79bb63a7
Theo Nga <theo@everythingzimbabwean.com>
2025-02-18 19:41:49 +0200

    Update ALLOWED_HOSTS in settings and add python-decouple to requirements

--- bankrates/settings.py
+++ bankrates/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "https://bank-rates.onrender.com",
+    "localhost",
+    "127.0.0.1:8000"
+]
 
 
 # Application definition
